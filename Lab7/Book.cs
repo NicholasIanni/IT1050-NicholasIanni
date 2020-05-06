c263bc9d116b5c26a182bee34838120c5709caec
NicholasIanni <62266432+NicholasIanni@users.noreply.github.com>
2020-05-06 18:03:40 -0400

    Add files via upload

---
+++ Lab7/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Book
+{
+	public String Title;
+	public String Author;
+	public int Year;
+	public void Display()
+	{
+		Console.WriteLine($"Title: {Title}");
+		Console.WriteLine($"Author: {Author}");
+	}
+	public Book()
+	{
+	}
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Author = "Ianni";
+            book.Title = "Get Rich Quick";
+            book.Year = 2020;
+
+            book.Display();
+        }
+    }
+}
